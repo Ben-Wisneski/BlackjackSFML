3c5cdf5d423c7ae1aa37c3a77d0ca9f0b5b77c13
Ben-Wisneski <wisnesbs@mail.uc.edu>
2026-01-23 13:45:40 -0500

    initial commit: adding main.cpp and .gitignore new file:   .gitignore new file:   main.cpp

---
+++ main.cpp
@@ -0,0 +1,65 @@
+#include <SFML/Graphics.hpp>
+#include <iostream>
+
+int main()
+{
+    sf::RenderWindow window(sf::VideoMode::getDesktopMode(), "Practice");
+    window.setFramerateLimit(60);
+    sf::RectangleShape shape(sf::Vector2f(50.f, 150.f));
+    sf::RectangleShape bro(sf::Vector2f(50.f, 150.f));
+    bro.setFillColor(sf::Color::Blue);
+    bro.move({600.f,0});
+    while (window.isOpen())
+    {
+        while (const std::optional event = window.pollEvent())
+        {
+            if (event->is<sf::Event::Closed>())
+                window.close();
+            //Used the getIf function built into sf::Event to check if escape key is pressed and if it is, closes the window
+            else if (const auto* keyPressed = event->getIf<sf::Event::KeyPressed>())
+            {
+                if (keyPressed->scancode == sf::Keyboard::Scancode::Escape)
+                {    
+                    std::cout<<"esc pressed\n";
+                    window.close();
+                }
+                else if(keyPressed-> scancode == sf::Keyboard::Scancode::W)
+                {
+                    std::cout<<"W pressed!\n";
+                    shape.move({0.f, -100.f});
+                    
+                }
+                else if(keyPressed-> scancode == sf::Keyboard::Scancode::A)
+                {
+                    std::cout<<"A pressed!\n";
+                    shape.move({-100.f, 0.f});
+                }
+                else if(keyPressed-> scancode == sf::Keyboard::Scancode::S)
+                {   
+                    std::cout<<"S pressed!\n";   
+                    shape.move({0.f, 100.f});
+                }
+                else if(keyPressed-> scancode==sf::Keyboard::Scancode::D)
+                {
+                    std::cout<<"D pressed!\n";  
+                    shape.move({100.f, 0.f});
+                }
+
+                //for the blue shape movement
+                if(keyPressed-> scancode == sf::Keyboard::Scancode::Up)
+                    bro.move({0.f, -100.f});
+                else if(keyPressed-> scancode == sf::Keyboard::Scancode::Left)
+                    bro.move({-100.f, 0.f});
+                else if(keyPressed-> scancode == sf::Keyboard::Scancode::Down)   
+                    bro.move({0.f, 100.f});
+                else if(keyPressed-> scancode==sf::Keyboard::Scancode::Right)
+                    bro.move({100.f, 0.f});
+            }
+            
+        }
+        window.clear();
+        window.draw(shape);
+        window.draw(bro);
+        window.display();
+    }
+}
